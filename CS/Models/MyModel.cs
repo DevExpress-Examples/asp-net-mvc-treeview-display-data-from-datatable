25f66bcf4615b8d7ad3148125be96884c794ca44
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Models/MyModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace Example.Models {
+    public static class MyModel {
+        private static DataTable _MyDataTable;
+        public static DataTable MyDataTable {
+            get {
+                if (_MyDataTable == null) {
+                    _MyDataTable = new DataTable();
+
+                    _MyDataTable.Columns.Add("ID", typeof(System.Int32));
+                    _MyDataTable.Columns.Add("Title", typeof(System.String));
+                    _MyDataTable.Columns.Add("ParentID", typeof(System.Int32));
+
+                    _MyDataTable.PrimaryKey = new DataColumn[] { _MyDataTable.Columns["ID"] };
+
+                    _MyDataTable.Rows.Add(1, "Nokia", 0);
+                    _MyDataTable.Rows.Add(2, "N8", 1);
+                    _MyDataTable.Rows.Add(3, "N91", 1);
+                    _MyDataTable.Rows.Add(4, "Samsung", 0);
+                    _MyDataTable.Rows.Add(5, "Corby9", 4);
+                    _MyDataTable.Rows.Add(6, "Star", 0);
+                }
+
+                return _MyDataTable;
+            }
+        }
+    }
+}
---
+++ VB/Controllers/HomeController.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Mvc
+Imports Example.Models
+Imports DevExpress.Web.Mvc
+Imports System.Data
+Imports DevExpress.Web.ASPxTreeView
+
+Namespace Example.Controllers
+	Public Class HomeController
+		Inherits Controller
+		Public Function Index() As ActionResult
+			ViewBag.Message = "Welcome to DevExpress Extensions for ASP.NET MVC!"
+
+			Return View(MyModel.MyDataTable)
+		End Function
+
+		Public Shared Sub CreateTreeViewNodesRecursive(ByVal model As DataTable, ByVal nodesCollection As MVCxTreeViewNodeCollection, ByVal parentID As Int32)
+			Dim rows = model.Select("[ParentID] = " & parentID)
+
+			For Each row As DataRow In rows
+				Dim node As MVCxTreeViewNode = nodesCollection.Add(row("Title").ToString(), row("ID").ToString())
+				CreateTreeViewNodesRecursive(model, node.Nodes, Convert.ToInt32(row("ID")))
+			Next row
+		End Sub
+	End Class
+
+End Namespace
